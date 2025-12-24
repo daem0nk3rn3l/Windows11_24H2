97f7e55b240ffc4d2cdc1c84bfdd6954dec55d46
Linda <118927607+daem0nk3rn3l@users.noreply.github.com>
2025-12-24 11:34:26 -0500

    Add PowerShell script for computer info report

---
+++ ComputerInfo.ps1
@@ -0,0 +1,31 @@
+# Computer Information Output to HTML no headers
+# 1.19.2023
+#
+#
+
+
+#The command below will get the name of the computer
+$ComputerName = "<h1>Computer name: $env:computername</h1>"
+
+#The command below will get the Operating System information, convert the result to HTML code as table and store it to a variable
+$OSinfo = Get-CimInstance -Class Win32_OperatingSystem | ConvertTo-Html -As List -Property Version,Caption,BuildNumber,Manufacturer -Fragment -PreContent "<h2>Operating System Information</h2>"
+
+#The command below will get the Processor information, convert the result to HTML code as table and store it to a variable
+$ProcessInfo = Get-CimInstance -ClassName Win32_Processor | ConvertTo-Html -As List -Property DeviceID,Name,Caption,MaxClockSpeed,SocketDesignation,Manufacturer -Fragment -PreContent "<h2>Processor Information</h2>"
+
+#The command below will get the BIOS information, convert the result to HTML code as table and store it to a variable
+$BiosInfo = Get-CimInstance -ClassName Win32_BIOS | ConvertTo-Html -As List -Property SMBIOSBIOSVersion,Manufacturer,Name,SerialNumber -Fragment -PreContent "<h2>BIOS Information</h2>"
+
+#The command below will get the details of Disk, convert the result to HTML code as table and store it to a variable
+$DiscInfo = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | ConvertTo-Html -As List -Property DeviceID,DriveType,ProviderName,VolumeName,Size,FreeSpace -Fragment -PreContent "<h2>Disk Information</h2>"
+  
+
+
+#The command below will combine all the information gathered into a single HTML report
+$Report = ConvertTo-HTML -Body "$ComputerName $OSinfo $ProcessInfo $BiosInfo $DiscInfo" `
+-Title "Computer Information" -Head $header -PostContent "<p>Creation Date: $(Get-Date)<p>"
+#The command below will generate the report to an HTML file
+$Report | Out-File .\Computer-Info.html
+
+
+
